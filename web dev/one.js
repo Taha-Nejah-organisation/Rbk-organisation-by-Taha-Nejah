04a4e15335d283a91798e7e8651534b3c0af99e9
blusk1n <tahaznazen@gmail.com>
2019-10-28 15:36:32 +0100

    added by nejah & taha

---
+++ web dev/one.js
@@ -0,0 +1,86 @@
+/*1*/
+$('img').attr('src','https://cdn.vox-cdn.com/thumbor/Or0rhkc1ciDqjrKv73IEXGHtna0=/0x0:666x444/1200x800/filters:focal(273x193:379x299)/cdn.vox-cdn.com/uploads/chorus_image/image/59384673/Macaca_nigra_self-portrait__rotated_and_cropped_.0.jpg')
+/*2*/
+$('button#go').on('click', function(){ $('p#output').text( $('input#color').val() ).css('color', 'red'); });
+/*===> it take the value (using .val)inside the input field and change his color to red using(.css)
+and replace it with the value p tag with outpout id using (.text) */
+/*3*/
+$('button#go').on('click',function(){alert("hello")})
+
+/*4*/
+//append() insert content to the end of an element
+/*5*/
+$('#secret').hide()
+/*6*/
+// body is not a string
+/*7*/
+$('body').css('background-color', 'red');
+/*8*/
+$('li#three').append('<li> things four</li>')
+
+
+// ------------------web development2-------------------------
+/*1*/
+//Bob says hi, immediately
+/*2*/
+window.sayHello.call(alice)
+/*3*/
+20
+/*4*/
+"Hi I am window"
+/*5*/
+20
+/*6*/
+//Alice says hi, after 1 second
+/*7*/
+/*modularize our code for reuse and composition
+centralize code to simplify maintenance
+reduce errors and repetition*/
+
+
+//----------------GIT ---------------------------------------
+
+//1
+//git clone
+//2
+//git status
+//3
+//rm *.json
+//4
+//rm -rf config
+//5
+//6
+//git remote
+//7
+//Using the "mv" command, I move config to setup.
+//8
+//cd ..
+//9
+//cd ../../public
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
